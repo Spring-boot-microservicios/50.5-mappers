0a2b8382113bf84856112ef11eef480619bc0b81
Luis Angel <angelfgdeveloper@gmail.com>
2024-10-06 20:43:55 -0600

    Uso de mapper con mappings de entity a dto

--- src/main/java/com/angelfg/mapstruct/mappers/CountryMapper.java
+++ src/main/java/com/angelfg/mapstruct/mappers/CountryMapper.java
@@ -1,8 +1,11 @@
 package com.angelfg.mapstruct.mappers;
 
 import com.angelfg.mapstruct.dtos.CountryDto;
+import com.angelfg.mapstruct.dtos.LanguageDto;
 import com.angelfg.mapstruct.models.Country;
+import com.angelfg.mapstruct.models.Language;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,6 +13,12 @@ public interface CountryMapper {
 
     CountryMapper mapper = Mappers.getMapper(CountryMapper.class);
 
+    @Mapping(target = "continent", source = "location.continent")
     CountryDto toCountryDto(Country country);
+
+    // Del dto target (isOfficialLanguage) al source entity (isOfficial)
+    @Mapping(target = "isOfficialLanguage", source = "isOfficial")
+    @Mapping(target = "speakersTotal", source = "speakersCount")
+    LanguageDto toLanguageDto(Language language);
 
 }
